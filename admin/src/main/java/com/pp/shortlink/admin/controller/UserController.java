e4eb6aabf6edb017acc5fe4e3014bf86d3a80ad3
PpSh1t <131457643+PpSh1t@users.noreply.github.com>
2025-03-31 20:20:28 +0800

    feature:开发短链接后管系统相关功能

--- admin/src/main/java/com/pp/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/pp/shortlink/admin/controller/UserController.java
@@ -2,7 +2,6 @@ package com.pp.shortlink.admin.controller;
 
 import com.pp.shortlink.admin.common.convention.result.Result;
 import com.pp.shortlink.admin.common.convention.result.Results;
-import com.pp.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.pp.shortlink.admin.dto.resp.UserRespDTO;
 import com.pp.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -25,11 +24,6 @@ public class UserController {
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
-        UserRespDTO result = userService.getUserByUsername(username);
-        if (result == null) {
-            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
-        }else{
-            return Results.success(result);
-        }
+        return Results.success(userService.getUserByUsername(username));
     }
 }
