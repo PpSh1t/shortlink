568d798b108a9f4a7b193c66f645cc6f1991a8d9
PpSh1t <131457643+PpSh1t@users.noreply.github.com>
2025-04-21 17:23:21 +0800

    feature:短链接分页接口新增创建时间以及时间序列化

--- project/src/main/java/com/pp/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
+++ project/src/main/java/com/pp/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -59,8 +60,15 @@ public class ShortLinkPageRespDTO extends Page {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @TableField(value = "valid_date")
     private Date validDate;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
 
     /**
      * 描述
