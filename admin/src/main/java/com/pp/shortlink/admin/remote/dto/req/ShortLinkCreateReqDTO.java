6a835586fea9d2dd665722c7355a0f6b10393dc2
PpSh1t <131457643+PpSh1t@users.noreply.github.com>
2025-04-24 17:11:49 +0800

    feature:前端传递有效期时间序列化

--- admin/src/main/java/com/pp/shortlink/admin/remote/dto/req/ShortLinkCreateReqDTO.java
+++ admin/src/main/java/com/pp/shortlink/admin/remote/dto/req/ShortLinkCreateReqDTO.java
@@ -2,6 +2,7 @@ package com.pp.shortlink.admin.remote.dto.req;
 
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -45,6 +46,7 @@ public class ShortLinkCreateReqDTO {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     @TableField(value = "valid_date")
     private Date validDate;
 
