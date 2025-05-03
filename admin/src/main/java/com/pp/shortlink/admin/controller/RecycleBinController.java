d7909fa7affc180b46eb5d85c3df40eeb9d60fb1
PpSh1t <131457643+PpSh1t@users.noreply.github.com>
2025-05-03 19:00:56 +0800

    feature:开发短链接新增回收站功能

---
+++ admin/src/main/java/com/pp/shortlink/admin/controller/RecycleBinController.java
@@ -0,0 +1,33 @@
+package com.pp.shortlink.admin.controller;
+
+import com.pp.shortlink.admin.common.convention.result.Result;
+import com.pp.shortlink.admin.common.convention.result.Results;
+import com.pp.shortlink.admin.remote.ShortLinkRemoteService;
+import com.pp.shortlink.admin.remote.dto.req.RecycleBinSaveReqDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 回收站管理控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class RecycleBinController {
+
+    /**
+     * 后续重构为SpringCloud Feign 调用
+     */
+    ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
+    };
+
+    /**
+     * 保存回收站
+     */
+    @PostMapping("/api/short-link/v1/recycle-bin/save")
+    public Result<Void> saveRecycleBin(@RequestBody RecycleBinSaveReqDTO requestParam) {
+        shortLinkRemoteService.saveRecycleBin(requestParam);
+        return Results.success();
+    }
+}
--- admin/src/main/java/com/pp/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/com/pp/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.TypeReference;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.pp.shortlink.admin.common.convention.result.Result;
+import com.pp.shortlink.admin.remote.dto.req.RecycleBinSaveReqDTO;
 import com.pp.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
 import com.pp.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.pp.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
@@ -89,5 +90,15 @@ public interface ShortLinkRemoteService {
         String resultStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/title?url=" + url);
         return JSON.parseObject(resultStr, new TypeReference<>() {
         });
+    }
+
+    /**
+     * 保存回收站
+     *
+     * @param requestParam 请求参数
+     * @return
+     */
+    default void saveRecycleBin(RecycleBinSaveReqDTO requestParam) {
+        HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/recycle-bin/save", JSON.toJSONString(requestParam));
     }
 }
