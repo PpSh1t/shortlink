ac7e825397098006e63e4eb248069c14ea6be1b6
PpSh1t <131457643+PpSh1t@users.noreply.github.com>
2025-05-15 16:39:21 +0800

    feature:开发单个短链接访问监控详情功能

--- project/src/main/java/com/pp/shortlink/project/dao/mapper/LinkNetworkStatsMapper.java
+++ project/src/main/java/com/pp/shortlink/project/dao/mapper/LinkNetworkStatsMapper.java
@@ -2,8 +2,12 @@ package com.pp.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.pp.shortlink.project.dao.entity.LinkNetworkStatsDO;
+import com.pp.shortlink.project.dto.req.ShortLinkStatsReqDTO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * 访问网络监控持久层
@@ -30,4 +34,19 @@ public interface LinkNetworkStatsMapper extends BaseMapper<LinkNetworkStatsDO> {
     """)
     void shortLinkNetworkState(@Param("linkNetworkStats") LinkNetworkStatsDO linkNetworkStatsDO);
 
+    /**
+     * 根据短链接获取指定日期内访问网络监控数据
+     */
+    @Select("SELECT " +
+            "    network, " +
+            "    SUM(cnt) AS cnt " +
+            "FROM " +
+            "    t_link_network_stats " +
+            "WHERE " +
+            "    full_short_url = #{param.fullShortUrl} " +
+            "    AND gid = #{param.gid} " +
+            "    AND date BETWEEN #{param.startDate} and #{param.endDate} " +
+            "GROUP BY " +
+            "    full_short_url, gid, network;")
+    List<LinkNetworkStatsDO> listNetworkStatsByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
 }
